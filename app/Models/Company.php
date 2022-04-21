cd1df38c325e41c26fbb2b910e53390d73844fb3
Haidar Al-fallouji <50954742+haid45@users.noreply.github.com>
2022-04-21 23:24:57 +1000

    Mail notification on company creation

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,18 +2,23 @@
 
 namespace App\Models;
 
+use App\Notifications\CompanyCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\File;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory,Notifiable;
     protected $fillable = ['name','email','logo','website'];
 
 
     protected static function boot() {
         parent::boot();
+        static::created(function(Company $company){
+            $company->notify(new CompanyCreated($company));
+        });
         static::deleted(function(Company $company){
             if (File::exists(public_path($company->logo))) {
                 File::delete(public_path($company->logo));
---
+++ app/Notifications/CompanyCreated.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Company;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CompanyCreated extends Notification
+{
+    use Queueable;
+
+    private Company $company;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Company $company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $company = $this->company;
+        return (new MailMessage)
+                    ->line('Welcome '.$company->name.' to '.env('APP_NAME', 'Laravel').'.')
+                    ->line('Click below to view the company.')
+                    ->action('View Company', route('companies.show',$company))
+                    ->line('We\'re excited to have you onboard');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
