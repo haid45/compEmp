82d0846b40c58d58e9f4697a7cb64dbaa1d6e7b2
Haidar Al-fallouji <50954742+haid45@users.noreply.github.com>
2022-04-21 14:39:04 +1000

    User seeder & test

---
+++ tests/Feature/SeederTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SeederTest extends TestCase
+{
+//    use RefreshDatabase;
+    /**
+     * Test seeding DB.
+     *
+     * @return void
+     */
+    public function testSeedingDB()
+    {
+        $this->seed();
+        $this->assertDatabaseHas('users', ['email'=> env('INITIAL_USER_EMAIL','admin@admin.com')]);
+    }
+}
