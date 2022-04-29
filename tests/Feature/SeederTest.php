b5c7d4281fcf31f9f6e7e219df14dc329658f46b
Haidar Al-fallouji <50954742+haid45@users.noreply.github.com>
2022-04-29 23:17:11 +1000

    Update SeederTest.php

--- tests/Feature/SeederTest.php
+++ tests/Feature/SeederTest.php
@@ -2,13 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class SeederTest extends TestCase
 {
-    use RefreshDatabase;
     /**
      * Test seeding DB.
      *
